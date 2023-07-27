2613aa18a7a6f8fc1a65b23380d633573eb988e8
UncankVladimirVayneChoque <139934081+UncankVladimirVayneChoque@users.noreply.github.com>
2023-07-27 17:33:17 -0400

    Add files via upload

---
+++ work_space_test_kr16_2/build/kuka_kr16_moveit/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/vayne/Documents/programa_expertos_en_ros/clase_5/work_space_test_kr16_2/src/kuka_kr16_moveit
+# Build directory: /home/vayne/Documents/programa_expertos_en_ros/clase_5/work_space_test_kr16_2/build/kuka_kr16_moveit
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
