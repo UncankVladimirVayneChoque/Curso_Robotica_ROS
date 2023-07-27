5da9377970c0ca45e4cd4fc6a352a899e91dc3df
UncankVladimirVayneChoque <139934081+UncankVladimirVayneChoque@users.noreply.github.com>
2023-07-27 17:21:44 -0400

    Delete build directory

--- build/paquete1/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vayne/Documents/programa_expertos_en_ros/clase_4/ws_practica3/src/paquete1
-# Build directory: /home/vayne/Documents/programa_expertos_en_ros/clase_4/ws_practica3/build/paquete1
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/paquete5/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vayne/Documents/programa_expertos_en_ros/clase_4/ws_practica3/src/paquete5
-# Build directory: /home/vayne/Documents/programa_expertos_en_ros/clase_4/ws_practica3/build/paquete5
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
