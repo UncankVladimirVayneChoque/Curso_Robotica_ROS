f7a46d3a790ec9121a442a89f242cab59c20e7e7
UncankVladimirVayneChoque <139934081+UncankVladimirVayneChoque@users.noreply.github.com>
2023-07-27 17:31:57 -0400

    Add files via upload

---
+++ work_space_test_kr16_2/build/CTestTestfile.cmake
@@ -0,0 +1,11 @@
+# CMake generated Testfile for 
+# Source directory: /home/vayne/Documents/programa_expertos_en_ros/clase_5/work_space_test_kr16_2/src
+# Build directory: /home/vayne/Documents/programa_expertos_en_ros/clase_5/work_space_test_kr16_2/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("basic_simulation")
+subdirs("kuka_kr16_moveit")
+subdirs("kuka_resources")
+subdirs("kuka_kr16_support")
---
+++ work_space_test_kr16_2/build/gtest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest/googlemock
+# Build directory: /home/vayne/Documents/programa_expertos_en_ros/clase_5/work_space_test_kr16_2/build/gtest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
