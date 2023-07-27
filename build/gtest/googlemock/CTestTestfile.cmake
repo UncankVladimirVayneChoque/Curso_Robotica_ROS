ec2c41e9eabfe9c756bccb0c21470a34fecfe410
UncankVladimirVayneChoque <139934081+UncankVladimirVayneChoque@users.noreply.github.com>
2023-07-27 15:21:26 -0400

    Add files via upload

---
+++ build/gtest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest/googlemock
+# Build directory: /home/vayne/Documents/programa_expertos_en_ros/clase_4/ws_practica3/build/gtest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
