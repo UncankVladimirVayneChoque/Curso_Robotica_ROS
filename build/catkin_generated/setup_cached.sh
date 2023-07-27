ad07fd023c35fc61da283a8e41f27c97d2b9f42e
UncankVladimirVayneChoque <139934081+UncankVladimirVayneChoque@users.noreply.github.com>
2023-07-27 15:18:02 -0400

    build

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export PWD='/home/vayne/Documents/programa_expertos_en_ros/clase_4/ws_practica3/build'
