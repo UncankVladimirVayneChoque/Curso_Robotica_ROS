1e1c9621653ba9ab33d009576ebceb647fad2cf9
UncankVladimirVayneChoque <139934081+UncankVladimirVayneChoque@users.noreply.github.com>
2023-07-27 15:22:36 -0400

    Add files via upload

---
+++ build/paquete3/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/vayne/Documents/programa_expertos_en_ros/clase_4/ws_practica3/src/paquete3
+# Build directory: /home/vayne/Documents/programa_expertos_en_ros/clase_4/ws_practica3/build/paquete3
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
