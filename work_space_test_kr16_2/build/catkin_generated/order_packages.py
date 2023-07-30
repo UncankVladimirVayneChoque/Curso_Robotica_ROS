5366482d395dfbc9f07548c05f3376f5612e177e
UncankVladimirVayneChoque <139934081+UncankVladimirVayneChoque@users.noreply.github.com>
2023-07-30 12:07:10 -0400

    Delete work_space_test_kr16_2 directory

--- work_space_test_kr16_2/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/vayne/Documents/programa_expertos_en_ros/clase_5/work_space_test_kr16_2/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/vayne/Documents/programa_expertos_en_ros/clase_5/work_space_test_kr16_2/devel;/opt/ros/noetic'.split(';') if '/home/vayne/Documents/programa_expertos_en_ros/clase_5/work_space_test_kr16_2/devel;/opt/ros/noetic' != '' else []
--- work_space_test_kr16_2/build/gtest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googletest
-# Build directory: /home/vayne/Documents/programa_expertos_en_ros/clase_5/work_space_test_kr16_2/build/gtest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
